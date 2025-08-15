141f0f475de10b40f4c75a91ea70c8b29dda9ae2
jamespickering19832 <james@aberalp.co.uk>
2025-08-15 09:41:02 +0100

    \"Fix login route order\"

--- app/routes.py
+++ app/routes.py
@@ -14,6 +14,25 @@ from app.db_routes import role_required
 def index():
     return render_template('index.html', title='Home')
 
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password')
+            return redirect(url_for('login'))
+        login_user(user, remember=form.remember_me.data)
+        return redirect(url_for('index'))
+    return render_template('login.html', title='Sign In', form=form)
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
+
 @app.route('/company', methods=['GET', 'POST'])
 def company():
     company = Company.query.first()
@@ -92,22 +111,3 @@ def admin_reset_data():
         db.session.rollback()
         flash(f'Error resetting data: {str(e)}', 'danger')
     return redirect(url_for('admin'))
-
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    if current_user.is_authenticated:
-        return redirect(url_for('index'))
-    form = LoginForm()
-    if form.validate_on_submit():
-        user = User.query.filter_by(username=form.username.data).first()
-        if user is None or not user.check_password(form.password.data):
-            flash('Invalid username or password')
-            return redirect(url_for('login'))
-        login_user(user, remember=form.remember_me.data)
-        return redirect(url_for('index'))
-    return render_template('login.html', title='Sign In', form=form)
-
-@app.route('/logout')
-def logout():
-    logout_user()
-    return redirect(url_for('index'))
